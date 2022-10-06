517ddeb3edb6f445c43554976df3d2e60cfac3a8
Davi Bogo <bogodavi10@gmail.com>
2022-10-06 20:53:23 -0300

    adicionando tela e alguns elementos da thread

---
+++ src/main/java/udesc/traffic/simulator/controller/CarroController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udesc.traffic.simulator.controller;
+
+/**
+ *
+ * @author davib
+ */
+public class CarroController {
+    
+}
---
+++ src/main/java/udesc/traffic/simulator/model/Carro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udesc.traffic.simulator.model;
+
+/**
+ *
+ * @author davib
+ */
+public class Carro extends Thread {
+    @Override
+    public void run() {
+        
+    }
+}
